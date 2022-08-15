86eb4551244ad08048750cb1d623399ef83a38a2
Justin <justin.kiarie@student.moringaschool.com>
2022-08-15 09:52:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+Cards = (["Sam", "Brenda", "Ada" ,"Ali"]);
+function wrapGifts(Cards) {
+    for (let i = 0; i < Cards.length; i++) {
+      console.log(`Thank you, ${Cards[i]}, for the wonderful birthday gift!`);
+    }
+    return Cards;
+    
+  }
+  wrapGifts(Cards);// Code your solutions in this file
